fabc968cd22d806eca749d856b88b94a2eca40c5
TaoistBear <originaltaoistbear@gmail.com>
2016-10-14 09:05:13 -0500

    Initial Git Commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// 11 STEPS FOR PROJECT COPLETION
+
+// Create and lin a JS file to index.html - *** DONE
+
+// Set focus on the first text field
+
+// Reveal text field for Job Role: Other selection
+
+// Pair T-Shirt color selectins dynamically
+    //Hide/Show T-Shirt options dynamically through selection
+
+// Calcualte Registration total and block out conflicts
+
+// Dynamic pay info selection
+
+// Display error messages for incomplete data
+
+// Clear any syntax errors
+
+// Syle selecttion menus to match page
+
+// Add Credit Card Valition
