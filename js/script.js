34d568ea4c27a3838998747874fa8882ffb8e291
TaoistBear <originaltaoistbear@gmail.com>
2016-10-15 11:43:34 -0400

    Down to Shirts
    Down to the shirt options

--- js/script.js
+++ js/script.js
@@ -7,9 +7,37 @@ $(document).ready(function() {
   $('#name').focus();
 });
 // Reveal text field for Job Role: Other selection
+var otherField = $('<input type="text" id="other-title" placeholder="Your Title">');
+$('fieldset:eq(0)').append(otherField);
+$('#other-title').hide();
+$('#title').on('change', function() {
+  if ($('#title option:selected').val() === 'other') {
+    $('#other-title').show();
+  }
+});
 
-// Pair T-Shirt color selectins dynamically
-    //Hide/Show T-Shirt options dynamically through selection
+//Hide/Show T-Shirt options dynamically through selection ***DONE
+var $shirtDesign = $('.shirt div:eq(1)');
+var $shirtColorDiv = $('.shirt div:eq(2)');
+var $shirtColorOptions = $('#color option');
+$shirtColorDiv.hide();
+$shirtColorOptions.hide();
+$shirtDesign.on('change', function() {
+  // Pair T-Shirt color selectins dynamically
+  if ($('#design option:selected').val() === 'js puns') {
+    $shirtColorOptions.hide();
+    $('#color option:eq(0)').show();
+    $('#color option:eq(1)').show();
+    $('#color option:eq(2)').show();
+  } else {
+    $shirtColorOptions.hide();
+    $('#color option:selected').val('tomato');
+    $('#color option:eq(3)').show();
+    $('#color option:eq(4)').show();
+    $('#color option:eq(5)').show();
+  }
+  $shirtColorDiv.show();
+});
 
 // Calcualte Registration total and block out conflicts
 
